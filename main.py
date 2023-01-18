36f4762abd21e52fdf42222d7242e333589764f7
fallnomega <fallnomega@gmail.com>
2023-01-18 12:43:46 -0800

    code reformat

--- main.py
+++ main.py
@@ -33,7 +33,8 @@ def decrypt(message, shifting):
     print (f"The decoded text is {decoded}")
     return
 
-
+#TODO-1: Combine the encrypt() and decrypt() functions into a single function called caesar().
+#TODO-2: Call the caesar() function, passing over the 'text', 'shift' and 'direction' values.
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 # direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 direction="decode"
