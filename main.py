274d1e00429214db60191b5cf526c99647480118
fallnomega <fallnomega@gmail.com>
2023-01-18 11:54:06 -0800

    decode working.... i think

--- main.py
+++ main.py
@@ -2,32 +2,53 @@ def encrypt(message, shifting):
     testing_original = ""
     encrypted = ""
     for x in message:
-        testing_original+=x
-        temp_position =  (alphabet.index(x))
+        testing_original += x
+        temp_position = (alphabet.index(x))
         if temp_position + int(shifting) > 25:
-            reverse_index_pos =  ((temp_position + int(shifting))-25)
-            encrypted += alphabet[reverse_index_pos-1]
+            reverse_index_pos = ((temp_position + int(shifting)) - 25)
+            encrypted += alphabet[reverse_index_pos - 1]
             # alphabet.reverse()
             # print(alphabet)
-
-
         else:
             encrypted+=alphabet[temp_position + int(shifting)]
     print (f"The original text is {testing_original}")
     print (f"The encoded text is {encrypted}")
 
+def decrypt(message, shifting):
+    testing_original = ""
+    decoded = ""
+    # print (f"message: {message}")
+    for x in message:
+        testing_original+=x
+        temp_position =  (alphabet.index(x))
+        if temp_position + int(shifting) < 25:
+            reverse_index_pos = (temp_position - int(shifting))
+            # print(f"26 - temp position {temp_position} - {shifting} shifting = {reverse_index_pos} reverse_index_pos")
+
+            # print(reverse_index_pos)
+            decoded += alphabet[reverse_index_pos]
+        else:
+            decoded+=alphabet[temp_position - int(shifting)]
+    print (f"The encoded text is {testing_original}")
+    print (f"The decoded text is {decoded}")
+    return
 
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 # direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+direction="decode"
 # text = input("Type your message:\n").lower()
-text = "civilization".lower()
+# text = "ezqz".lower()
+text = "ezqz".lower()
 # shift = int(input("Type the shift number:\n"))
 shift = "5"
-encrypt(text,shift)
-#TODO-1: Create a different function called 'decrypt' that takes the 'text' and 'shift' as inputs.
+if direction=="encode":
+    encrypt(text,shift)
+elif direction=="decode":
+    decrypt (text,shift)
+else:
+    print(f"{direction} is invalid")
 
-  #TODO-2: Inside the 'decrypt' function, shift each letter of the 'text' *backwards* in the alphabet by the shift amount and print the decrypted text.
   #e.g.
   #cipher_text = "mjqqt"
   #shift = 5
@@ -35,7 +56,6 @@ encrypt(text,shift)
   #print output: "The decoded text is hello"
 
 
-#TODO-3: Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable. Then call the correct function based on that 'drection' variable. You should be able to test the code to encrypt *AND* decrypt a message.
 
 
 
