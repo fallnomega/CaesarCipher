ba204b19bd63dfad4485217c02a17498f09aa947
fallnomega <fallnomega@gmail.com>
2023-01-18 13:30:35 -0800

    code complete ... i hope :(

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 from art import logo
 
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z']
 
-def caeser(message,shifting,direction_to_go):
+
+def caeser(message, shifting, direction_to_go):
+    if int(shifting) > 26:
+        shifting = int(shifting) % 26
+
     if direction_to_go == "encode":
         testing_original = ""
         encrypted = ""
         for x in message:
             testing_original += x
             if x not in alphabet:
-                encrypted+=x
+                encrypted += x
             elif alphabet.index(x) + int(shifting) > 25:
                 reverse_index_pos = ((alphabet.index(x) + int(shifting)) - 25)
                 encrypted += alphabet[reverse_index_pos - 1]
@@ -25,53 +30,35 @@ def caeser(message,shifting,direction_to_go):
         # print (f"message: {message}")
         for x in message:
             testing_original += x
-            if alphabet.index(x) + int(shifting) < 25:
+            if x not in alphabet:
+                decoded += x
+            elif alphabet.index(x) + int(shifting) < 25:
                 reverse_index_pos = (alphabet.index(x) - int(shifting))
-
-                # print(f"26 - temp position {alphabet.index(x)} - {shifting} shifting = {reverse_index_pos} reverse_index_pos")
-
-                # print(reverse_index_pos)
                 decoded += alphabet[reverse_index_pos]
             else:
-                decoded += alphabet.index(x) - int(shifting)
+                decoded += alphabet[alphabet.index(x) - int(shifting)]
         print(f"The encoded text is {testing_original}")
         print(f"The decoded text is {decoded}")
     else:
         print(f"{direction_to_go} is invalid")
 
-print(45 % 26)
-
-#TODO-2: What if the user enters a shift that is greater than the number of letters in the alphabet?
-# Try running the program and entering a shift number of 45.
-# Add some code so that the program continues to work even if the user enters a shift number greater than 26.
-# Hint: Think about how you can use the modulus (%).
-
-#TODO-4: Can you figure out a way to ask the user if they want to restart the cipher program?
-# e.g. Type 'yes' if you want to go again. Otherwise type 'no'.
-# If they type 'yes' then ask them for the direction/text/shift again and call the caesar() function again?
-# Hint: Try creating a while loop that continues to execute the program if the user types 'yes'.
-
-
-# direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-direction="encode"
-# text = input("Type your message:\n").lower()
-# text = "ezqz".lower()
-text = "hello zulu".lower()
-# shift = int(input("Type the shift number:\n"))
-shift = "5"
-
-
-# print (logo)
-
-caeser(message=text,shifting=shift,direction_to_go=direction)
-
-  #e.g.
-  #cipher_text = "mjqqt"
-  #shift = 5
-  #plain_text = "hello"
-  #print output: "The decoded text is hello"
 
 
 
+print (logo)
+restart = True
+while restart:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    caeser(message=text, shifting=shift, direction_to_go=direction)
+    redo = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n").lower()
+    if redo == 'no':
+        print("Goodbye!")
+        restart = False
 
-
+# e.g.
+# cipher_text = "mjqqt"
+# shift = 5
+# plain_text = "hello"
+# print output: "The decoded text is hello"
