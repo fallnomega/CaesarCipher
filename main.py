dd80082021c68e3a43e618464ee5ceb155c87913
fallnomega <fallnomega@gmail.com>
2023-01-18 13:13:18 -0800

    addressing edge case bugs

--- main.py
+++ main.py
@@ -45,9 +45,6 @@ def caeser(message,shifting,direction_to_go):
 # Add some code so that the program continues to work even if the user enters a shift number greater than 26.
 # Hint: Think about how you can use the modulus (%).
 
-# TODO-3: What happens if the user enters a number/symbol/space?
-#  Can you fix the code to keep the number/symbol/space when the text is encoded/decoded?
-
 #TODO-4: Can you figure out a way to ask the user if they want to restart the cipher program?
 # e.g. Type 'yes' if you want to go again. Otherwise type 'no'.
 # If they type 'yes' then ask them for the direction/text/shift again and call the caesar() function again?
