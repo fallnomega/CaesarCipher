d1a5f820da5801671e3f4305ac5505acfd144cf9
fallnomega <fallnomega@gmail.com>
2023-01-18 10:23:36 -0800

    placeholder for cipher

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
+# TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.
+def encrypt(message, shifting):
+
+    return
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+# direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+# text = input("Type your message:\n").lower()
+text = "Final inspection done".lower()
+# shift = int(input("Type the shift number:\n"))
+shift = "1"
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+encrypt(text,shift)
 
-#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
 
-    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
-    #e.g. 
+    #e.g.
     #plain_text = "hello"
     #shift = 5
     #cipher_text = "mjqqt"
